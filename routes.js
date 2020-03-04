31423d13ad294be87b7dc14f67b83edea87a1959
kkeeth <zensin0082@gmail.com>
2020-03-05 01:22:34 +0900

    feat: devide routings logic

---
+++ routes.js
@@ -0,0 +1,63 @@
+import { Router } from 'express'
+import { fragments, renderAsyncFragments } from '@riotjs/ssr'
+import register from '@riotjs/ssr/register'
+import template from 'lodash.template'
+import { readFileSync } from 'fs'
+
+// register .riot extension
+register()
+
+// base page
+const page = readFileSync('./index.html', 'utf8')
+
+// router instance
+const router = Router()
+
+// SSR
+const ssr = async (file, obj) => {
+  try {
+    // load riot file
+    const RiotTag = require(`./pages/${file}.riot`).default
+    // rendering
+    const { html, css } = await renderAsyncFragments(RiotTag.name, RiotTag, obj)
+    return {
+      html: html,
+      css: css,
+      title: `${file} Page`
+    }
+  } catch (e) {
+    if (e.code === 'MODULE_NOT_FOUND') {
+      const NotFound = require('./pages/not-found.riot').default
+      return { ...fragments('not-found', NotFound, { params: e }), title: 'Not Found' }
+    }
+    else {
+      const Error = require('./pages/error.riot').default
+      return { ...fragments('error', Error, { params: e }), title: 'Error Page' }
+    }
+  }
+}
+
+// routers
+router.get('/', async (req, res) => {
+  const { html, css } = await ssr('app', {
+    title: 'Hello Riot with Express!!'
+  })
+  res.send(template(page)({
+    html,
+    css,
+    title: 'Riot App'
+  }))
+})
+
+router.get('/:page', async (req, res) => {
+  const { html, css, title } = await ssr(req.params.page, {
+    title: `${req.params.page} Page`
+  })
+  res.send(template(page)({
+    html,
+    css,
+    title: title
+  }))
+})
+
+export default router
--- server.js
+++ server.js
@@ -1,34 +1,7 @@
 import express from 'express'
-import { fragments, renderAsyncFragments } from '@riotjs/ssr'
-import register from '@riotjs/ssr/register'
-import template from 'lodash.template'
-import { readFileSync } from 'fs'
 import path from 'path'
 import favicon from 'serve-favicon'
-
-const page = readFileSync('./index.html', 'utf8')
-
-// register .riot extension
-register()
-
-// SSR
-const ssr = (file, obj) => {
-  try {
-    // load riot file
-    const RiotTag = require(`./pages/${file}.riot`).default
-    // rendering
-    return renderAsyncFragments(RiotTag.name, RiotTag, obj)
-  } catch (e) {
-    if (e.code === 'MODULE_NOT_FOUND') {
-      const NotFound = require('./pages/not-found.riot').default
-      return fragments('not-found', NotFound, { params: e })
-    }
-    else {
-      const Error = require('./pages/error.riot').default
-      return fragments('error', Error, { params: e })
-    }
-  }
-};
+import routes from './routes'
 
 // express instance
 const app = express()
@@ -42,23 +15,7 @@ app.set('etag', false)
 app.set('x-powered-by', false)
 
 // routing
-app.get('/', async (req, res) => {
-  const { html, css } = await ssr('hello', {})
-  res.send(template(page)({
-    html,
-    css,
-    title: 'Riot App'
-  }))
-})
-
-app.get('/:page', (req, res) => {
-  const { html, css } = ssr(req.params.page, {})
-  res.send(template(page)({
-    html,
-    css,
-    title: 'Error Page'
-  }))
-})
+app.use(routes)
 
 app.listen(8080, () => {
   console.log('server running on port: 8080')
